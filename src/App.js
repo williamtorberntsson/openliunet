138dba6e7a49b1f980897d3e4854bb6b3f2d61ce
wilto837 <wilto837@student.liu.se>
2023-02-08 11:43:16 +0100

    first version

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Button Schema'>
+        <a
+          className="Schema"
+          href="https://cloud.timeedit.net/liu/web/schema/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Schema
+        </a>
+      </div>
+      <div className='Button Printa'>
+        <a
+          className="Printa"
+          href="https://portalliu.onricoh.se/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Printa
+        </a>
+      </div>
+      <div className='Button Boka'>
+        <a
+          href="https://cloud.timeedit.net/liu/web/wr_stud/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Boka Sal
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
