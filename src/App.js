2fd491b4a84c05255933bc0e5ffbc2cd09f2f703
wilto837 <wilto837@student.liu.se>
2023-02-09 10:19:19 +0100

    added link to Github

--- src/App.js
+++ src/App.js
@@ -2,35 +2,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='Button Schema'>
-        <a
-          className="Schema"
-          href="https://cloud.timeedit.net/liu/web/schema/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Schema
-        </a>
+    <div className='Main'>
+      <div className="App">
+        <div className='Button Schema'>
+          <a
+            className="Schema"
+            href="https://cloud.timeedit.net/liu/web/schema/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Schema
+          </a>
+        </div>
+        <div className='Button Printa'>
+          <a
+            className="Printa"
+            href="https://portalliu.onricoh.se/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Printa
+          </a>
+        </div>
+        <div className='Button Boka'>
+          <a
+            href="https://cloud.timeedit.net/liu/web/wr_stud/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Boka Sal
+          </a>
+        </div>
       </div>
-      <div className='Button Printa'>
+      <div className="Github">
         <a
-          className="Printa"
-          href="https://portalliu.onricoh.se/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Printa
-        </a>
-      </div>
-      <div className='Button Boka'>
-        <a
-          href="https://cloud.timeedit.net/liu/web/wr_stud/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Boka Sal
-        </a>
+            href="https://github.com/williamtorberntsson/openliunet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github page to contribute! 
+          </a>
       </div>
     </div>
   );
