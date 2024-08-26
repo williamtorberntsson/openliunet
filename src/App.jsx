3182696a4b263827b46962ffa6bc9b438abd4afe
William Torberntsson <william.torberntsson@gmail.com>
2024-08-26 20:42:06 +0200

    Updated study periods

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import TerminsTider from './TerminsTider';
-import Karta from './Karta';
-import './App.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TerminsTider from "./TerminsTider";
+import Karta from "./Karta";
+import "./App.css";
 
 function App() {
 
@@ -10,11 +10,11 @@ function App() {
   const toggleTermin = () => setTermin(!termin);
 
   return (
-    <div className='Main'>
+    <div className="Main">
       <div className="App">
         <div className="Button">
           <Link to="https://cloud.timeedit.net/liu/web/schema/" target="_blank" rel="noopener noreferrer">
-            <div className='Text Schema'>
+            <div className="Text Schema Round">
               Schema
             </div>
           </Link>
@@ -22,7 +22,7 @@ function App() {
 
         <div className="Button">
           <Link to="https://portalliu.onricoh.se/" target="_blank" rel="noopener noreferrer">
-            <div className='Text Printa'>
+            <div className="Text Printa Circle">
               Printa
             </div>
           </Link>
@@ -30,7 +30,7 @@ function App() {
 
         <div className="Button">
           <Link to="https://cloud.timeedit.net/liu/web/wr_stud/" target="_blank" rel="noopener noreferrer">
-            <div className='Text Boka'>
+            <div className="Text Boka Round">
               Boka Sal
             </div>
           </Link>
@@ -38,7 +38,7 @@ function App() {
 
         <div className="Button">
           <Link to="https://ysektionen.se/student/tentastatistik/" target="_blank" rel="noopener noreferrer">
-            <div className='Text Tenta'>
+            <div className="Text Tenta Circle">
               Tentastatistik
               (tek, fil, med finns)
             </div>
@@ -47,14 +47,14 @@ function App() {
 
         <div className="Button">
           <Link to="https://minit.liu.se/" target="_blank" rel="noopener noreferrer">
-            <div className='Text MinIT'>
+            <div className="Text MinIT Round">
               MinIT
             </div>
           </Link>
         </div>
 
         <div className="Button" onClick={toggleTermin}>
-          <div className='Text Termin'>
+          <div className="Text Termin Circle">
             <u>Terminstider</u>
           </div>
         </div>
@@ -66,7 +66,7 @@ function App() {
       <TerminsTider show={termin} />
 
       <Link to="https://github.com/williamtorberntsson/openliunet" target="_blank" rel="noopener noreferrer">
-        <div className='Github'>
+        <div className="Github">
           Github page to contribute!
         </div>
       </Link>
--- src/TerminsTider.jsx
+++ src/TerminsTider.jsx
@@ -7,52 +7,47 @@ const TerminsTider = ({show}) => {
 
   return (
     <div className="Information">
+      <p><a target="_blank" rel="noopener noreferrer" href="https://liu.se/artikel/terminstider">Also available from the source</a></p>
       <div className="Tekfak">
-        <h2>TekFak Spring 2023</h2>
-        <p><a target="_blank" rel="noopener noreferrer" href="https://liu.se/organisation/liu/lith/terminstider-for-tekniska-hogskolan">Also available from the source</a></p>
-        <h2>TekFak Autumn 2023</h2>
+        <h2>TekFak Autumn 2024</h2>
         <ul>
-          <li>2023-08-28 (v 35) - 2024-01-13 (v 2)</li>
-          <li>15 Aug - 27 Aug: Re-examination period</li>
-          <li>28 Aug - 18 Oct: Study period HT1</li>
-          <li>19 Oct - 20 Oct: Self study</li>
-          <li>23 Oct - 28 Oct: Examination period HT1</li>
-          <li>30 Oct - 22 Dec: Study Period HT2</li>
-          <li>27 Dec - 29 Dec: Self study</li>
-          <li>2 Jan - 5 Jan: Re-examination period HT1+VT2</li>
-          <li>8 Jan - 13 Jan: Examination period HT2</li>
+          <li>2024-08-19 (v 34) - 2025-01-18 (v 3)</li>
+          <li>19 Aug - 31 Aug: Re-examination period</li>
+          <li>2 Sep - 3 Nov: Study period HT1</li>
+          <li>25 Oct - 3 Nov: (Re-)Examination period HT1</li>
+          <li>4 Nov - 19 Jan: Study Period HT2</li>
+          <li>7 Jan - 10 Jan: Re-examination period HT1+VT2</li>
+          <li>13 Jan - 19 Jan: Examination period HT2</li>
         </ul>
-        <h2>TekFak Spring 2024</h2>
+        <h2>TekFak Spring 2025</h2>
         <ul>
-          <li>15 Jan - 8 Mar: Study period VT1</li>
-          <li>11 Mar - 11 Mar: Self study</li>
-          <li>12 Mar - 15 Mar: Re-examination period HT2</li>
-          <li>18 Mar - 23 Mar: Examination period VT1</li>
-          <li>25 Mar - 22 May: Study Period VT2</li>
-          <li>23 May - 24 May: Self Study</li>
-          <li>27 May - 1 Jun: Examination period VT2</li>
-          <li>3 Jun - 5 Jun: Re-examination period VT1</li>
-          <li>7 Jun - 7 Jun: Re-examination period VT1</li>
+          <li>2025-01-20 (v 4) - 2025-06-12 (v 24)</li>
+          <li>20 Jan - 30 Mar: Study period VT1</li>
+          <li>17 Mar - 20 Mar: Re-examination period HT2</li>
+          <li>21 Mar - 30 Mar: Examination period VT1</li>
+          <li>31 Mar - 12 Jun: Study Period VT2</li>
+          <li>28 May - 5 Jun: Examination period VT2</li>
+          <li>9 Jun - 12 Jun: Re-examination period VT1</li>
         </ul>
       </div>
       <div className="FilFak">
-        <h2>FilFak Spring 2023</h2>
+        <h2>FilFak Autumn 2024</h2>
         <ul>
-          <li>2023-01-23 (v 4) – 2023-06-11 (v 23)</li>
+          <li>2024-08-19 (v 34) – 2025-01-19 (v 3)</li>
         </ul>
-        <h2>FilFak Autumn 2023</h2>
+        <h2>FilFak Spring 2025</h2>
         <ul>
-          <li>2023-08-21 (v 34) - 2024-01-21 (v 3)</li>
+          <li>2025-01-20 (v 4) - 2024-06-08 (v 23)</li>
         </ul>
       </div>
       <div className="MedFak">
-        <h2>MedFak Spring 2023</h2>
+        <h2>MedFak Autumn 2024</h2>
         <ul>
-          <li>2023-01-23 (v 4) - 2023-06-11 (v 23)</li>
+          <li>2024-08-26 (v 35) - 2025-01-12 (v 2)</li>
         </ul>
-        <h2>MedFak Autumn 2023</h2>
+        <h2>MedFak Spring 2025</h2>
         <ul>
-          <li>2023-08-28 (v 35) - 2024-01-14 (v 2)</li>
+          <li>2025-01-20 (v 4) - 2025-06-08 (v 23)</li>
         </ul>
       </div>
     </div>
